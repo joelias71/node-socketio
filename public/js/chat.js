86b72f203c0e7045b11f726f8392cce50e6fb9b2
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-05-05 13:58:22 -0500

    feat(event) Socket io events

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,2 +1,24 @@
 //connect to the server
-io()
+const socket = io()
+
+socket.on('message', (msg) => {
+    console.log(msg)
+})
+
+socket.on('countUpdated', (count) => {
+    console.log('The count has been updated ' + count)
+})
+
+document.querySelector('#increment').addEventListener('click', () => {
+    socket.emit('increment')
+})
+
+//implicit we have acces to (e) event argument
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    //prevent default behavior that is to refresh the browser
+    e.preventDefault()
+
+    const message = document.querySelector('input').value
+
+    socket.emit('sendMessage', message)
+})
--- src/index.js
+++ src/index.js
@@ -12,8 +12,24 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+let count = 1
+
+io.on('connection', (socket) => {
     console.log('New websocket connection')
+
+    socket.emit('message', 'Welcome !')
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        //emit one connection
+        //socket.emit('countUpdated', ++count)
+        //emit event to all connections
+        io.emit('countUpdated', ++count)
+    })
+
+    socket.on('sendMessage', msg => {
+        io.emit('message', msg)
+    })
 })
 
 server.listen(port, () => {
